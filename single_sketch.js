b7b14504faaf59f19310b49194339b257cace9a5
Hunter Rodriguez <acehunter96@gmail.com>
2020-09-27 21:00:06 -0700

    need to find the math and stop approximating with pi

--- single_sketch.js
+++ single_sketch.js
@@ -1,62 +1,76 @@
 // Single-sketch example
 
 function setup (){
-  bg = loadImage('bg.jpg');
-  createCanvas (800, 800);
-
-
-
-  let sides = ['A','B','C','D'];
-  side1 = random(sides);
-  side2 = random(sides);
-  while (side1 == side2){
-    side2 = random(sides);
-  }
-
-  switch (side1) {
-    case 'A':
-      startx = random(0,800);
-      starty = 0;
-      break;
-    case 'B':
-      startx = 800;
-      starty = random(0,800);
-      break;
-    case 'C':
-      startx = random(0,800);
-      starty = 800;
-      break;
-    case 'D':
-      startx = 0;
-      starty = random(0,800);
-      break;
-  }
-  switch (side2) {
-    case 'A':
-      endx = random(0,800);
-      endy = 0;
-      break;
-    case 'B':
-      endx = 800;
-      endy = random(0,800);
-      break;
-    case 'C':
-      endx = random(0,800);
-      endy = 800;
-      break;
-    case 'D':
-      endx = 0;
-      endy = random(0,800);
-      break;
-  }
+  createCanvas (1000, 1000);
 }
 
 function draw(){
-  background(bg);
-  fill(255);
-  noStroke();
-  stroke(255);
+  background(255);
+  startingx = 400;
+  startingy = 400;
+  RAD2DEG = 57.2957795130823209;
+  armlength = 200;
 
-  line(startx,starty,endx,endy);
+
+  angle = atan2((mouseX - startingx), (mouseY - startingy));
+  
+  if (angle < 0.0){
+    angle += TWO_PI;
+  }
+  angledeg = RAD2DEG * angle;
+
+
+  let d = int(dist(startingx, startingy, mouseX, mouseY));
+  text("d: " + d, 0, height/10);
+  if ( d <= armlength){
+    var mod = map(d, 0, armlength, PI/2, PI/3)
+    angle = angle + mod;
+  }
+  if ( d < (armlength * 2) && d > armlength){
+    if ( d > (armlength * 2)*0.75){
+      var mod = map(d, armlength, (armlength * 2), PI/3, PI/8)
+      angle = angle + mod;
+    }
+    var mod = map(d, armlength, (armlength * 2)*0.75, PI/3, PI/5)
+    angle = angle + mod;
+  }
+  if ( d == (armlength * 2)){
+    angle = angle
+  }
+  else{
+    angle = angle
+  }
+
+
+  arm1y = armlength * cos(angle) + startingx;
+  arm1x = armlength * sin(angle) + startingy;
+
+  ellipse(startingx, startingy, 25);
+  ellipse(startingx, startingy, (armlength * 4) );
+  ellipse(startingx, startingy, (armlength * 3));
+  ellipse(startingx, startingy, (armlength * 2));
+  line(startingx, startingy, arm1x, arm1y);
+
+
+
+  ellipse(arm1x, arm1y, 25);
+
+
+
+  angle1 = atan2((mouseX - arm1x), (mouseY - arm1y));
+  if (angle1 < 0.0){
+    angle1 += TWO_PI;
+  }
+  angledeg1 = RAD2DEG * angle;
+
+  arm2y = armlength * cos(angle1) + arm1y;
+  arm2x = armlength * sin(angle1) + arm1x;
+
+  line(arm1x, arm1y, arm2x, arm2y);
+  ellipse(arm2x, arm2y, 8);
+  text("X: "+mouseX, 0, height/4);
+  text("Y: "+mouseY, 0, height/2);
+  text("angle_degrees: " + angledeg, 0, height/6);
+  text("angle_radians: " + round(angle/PI) + "PI", 0, height/8);
 }
 
